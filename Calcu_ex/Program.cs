824e1d41f4f34545730351ee857b05050dc02f54
남정욱 <wjddnr3304@gmail.com>
2024-11-04 08:54:01 +0900

    초기 커밋.

---
+++ Calcu_ex/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Calcu_ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("첫번째 값을 입력하세요");
+            string value1= Console.ReadLine();
+
+            Console.Write("두번째 값을 입력하세요");
+            string value2 = Console.ReadLine();
+
+            double dvalue1 =Double.Parse(value1);
+            double dvalue2 =Double.Parse(value2);
+
+            Console.WriteLine("{0}+{1}={2}",dvalue1,dvalue2,dvalue1+dvalue2);
+            Console.WriteLine("{0}*{1}={2}",dvalue1,dvalue2,dvalue1*dvalue2);
+            Console.WriteLine("{0}-{1}={2}",dvalue1,dvalue2,dvalue1-dvalue2);
+            Console.WriteLine("{0}/{1}={2}",dvalue1,dvalue2,dvalue1/dvalue2);
+           Console.Read();
+        }
+    }
+}
